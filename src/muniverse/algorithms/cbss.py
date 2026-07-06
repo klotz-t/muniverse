1b6d85d13de8c15673df99a34720c32294caed4f
klotz-t <thomas.klotz@mechbau.uni-stuttgart.de>
2026-07-06 11:54:58 +0200

    Update cbss.py
    - Bug in init function (remove commas)

--- src/muniverse/algorithms/cbss.py
+++ src/muniverse/algorithms/cbss.py
@@ -462,9 +462,9 @@ class FastIcaCBSS(_BaseCBSS):
         self.ica_max_iter = ica_max_iter
         self.ica_tol  = ica_tol
         self.ica_orthogonalization = ica_orthogonalization
-        self.clip_source = clip_source,
-        self.clip_method = clip_method,
-        self.clip_value = clip_value,
+        self.clip_source = clip_source
+        self.clip_method = clip_method
+        self.clip_value = clip_value
         self.refinement_loop = refinement_loop
         self.refinement_min_spikes = refinement_min_spikes
         self.refinement_loss = refinement_loss
@@ -569,6 +569,7 @@ class FastIcaCBSS(_BaseCBSS):
                 ValueError("The specified initalization method is not implemented")
 
             # fastICA fixedpoint optimization
+            w = w / np.linalg.norm(w)
             w = self._fixed_point_alg(w, white_sig, i)         
 
             # Predict source and estimate the source quality
@@ -786,5 +787,4 @@ class FastIcaCBSS(_BaseCBSS):
         
         return G
     
-        
        
